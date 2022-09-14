3b071cb8e28d1552588f502ec4d4804ae0285b71
Nicka <nickgabriadze@gmail.com>
2022-09-14 11:18:52 +0400

    Added - made the design for shopping cart

--- src/components/shoppingCart.tsx
+++ src/components/shoppingCart.tsx
@@ -1,24 +1,38 @@
 import { nanoid } from '@reduxjs/toolkit';
 import { useAppSelector } from '../features/hooks';
-
-export default function ShoppingCart(){
+import '../css/shoppingCart.css';
+import { Link } from 'react-router-dom';
+export default function ShoppingCart() {
     const smartphones = useAppSelector(state => state.shoppingCartReducer.items);
 
 
-    return(
+    return (
         <>
-        {smartphones.map(eachPhone => {
+            <div className='shoppingCart-container'>
+                <div className='shoppingCart-header'>
+                    <h1>Shopping Cart</h1>
+                    <Link className='links' to='/'><h2>Home</h2></Link>
+                </div>
 
-            return (
-                <div key={nanoid()}>
-                 {eachPhone.id}
-                 {eachPhone.name}
-                 <img src={eachPhone.imageAddress}></img>
 
-                </div>  
-            )
-        })}
-        
+                <div id='items-container'>
+                    {smartphones.length > 0 ? smartphones.map(eachPhone => {
+
+                        return (
+                            <div key={nanoid()} className='shopping-cart-items'>
+                                
+                                <img alt={eachPhone.name + "Phone Picture"}src={eachPhone.imageAddress}></img>
+                                <div>{eachPhone.name}</div>
+                                <div>
+                                <button className='shopping-cart-buttons'>Remove</button>
+                                <button className='shopping-cart-buttons'>Buy</button>
+                                </div>
+                            </div>
+                        )
+                    }) : <h3 id='no-items'>You Have No items in your shopping cart 😔
+                        <p>Try Adding Some 😊</p></h3>}
+                </div>
+            </div>
         </>
     )
 }
